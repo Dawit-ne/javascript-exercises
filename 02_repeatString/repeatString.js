e3d4db99ceb8f5c818936fcec8b91e602fad6160
Dawit-ne <ovodawit@gmail.com>
2023-08-06 12:27:14 +0300

    finished the hello world and the repeat string ex.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num<0){
+        return 'ERROR';
+    }
+    var output = '';
+for(let i=0; i<num; i++){
+    
+    if (num>0){
+        output = output+ str;
+    }
+   
+}
+if(num=0){
+    output = '';
+}
+return output;
 };
 
 // Do not edit below this line
